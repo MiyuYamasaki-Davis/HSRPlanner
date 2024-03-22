78208ddd1633671189fd534b5b010de9c76938cd
MiyuYamasaki-Davis <gjyamaguchi@ucdavis.edu>
2024-03-22 11:43:33 -0700

    pics and planner update

--- src/components/FullDropdown.js
+++ src/components/FullDropdown.js
@@ -1,58 +1,111 @@
 import React, { useState } from "react";
 import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+import Image from "next/image";
+// import acheron from "../../public/images/HSR/acheron.png"
+import argenti from "../../public/images/HSR/argenti.png"
+import arlan from "../../public/images/HSR/arlan.png"
+import asta from "../../public/images/HSR/asta.png"
+// import aventurine from "../../public/images/HSR/aventurine.png"
+import bailu from "../../public/images/HSR/bailu.png"
+import blackswan from "../../public/images/HSR/blackswan.png"
+import blade from "../../public/images/HSR/blade.png"
+// import boothill from "../../public/images/HSR/boothill.png"
+import bronya from "../../public/images/HSR/bronya.png"
+import clara from "../../public/images/HSR/clara.png"
+import danheng from "../../public/images/HSR/danheng.png"
+import danhengil from "../../public/images/HSR/danhengil.png"
+import drratio from "../../public/images/HSR/drratio.png"
+import fuxuan from "../../public/images/HSR/fuxuan.png"
+// import gallagher from "../../public/images/HSR/gallagher.png"
+import gepard from "../../public/images/HSR/gepard.png"
+import guinaifen from "../../public/images/HSR/guinaifen.png"
+import hanya from "../../public/images/HSR/hanya.png"
+import herta from "../../public/images/HSR/herta.png"
+import himeko from "../../public/images/HSR/himeko.png"
+import hook from "../../public/images/HSR/hook.png"
+import huohuo from "../../public/images/HSR/huohuo.png"
+import jingyuan from "../../public/images/HSR/jingyuan.png"
+import jingliu from "../../public/images/HSR/jingliu.png"
+import kafka from "../../public/images/HSR/kafka.png"
+import luka from "../../public/images/HSR/luka.png"
+import luocha from "../../public/images/HSR/luocha.png"
+import lynx from "../../public/images/HSR/lynx.png"
+import march from "../../public/images/HSR/march.png"
+import misha from "../../public/images/HSR/misha.png"
+import natasha from "../../public/images/HSR/natasha.png"
+import pela from "../../public/images/HSR/pela.png"
+import qingque from "../../public/images/HSR/qingque.png"
+// import robin from "../../public/images/HSR/robin.png"
+import ruanmei from "../../public/images/HSR/ruanmei.png"
+import sampo from "../../public/images/HSR/sampo.png"
+import seele from "../../public/images/HSR/seele.png"
+import serval from "../../public/images/HSR/serval.png"
+import silverwolf from "../../public/images/HSR/silverwolf.png"
+import sparkle from "../../public/images/HSR/sparkle.png"
+import sushang from "../../public/images/HSR/sushang.png"
+import tingyun from "../../public/images/HSR/tingyun.png"
+import topaz from "../../public/images/HSR/topaz.png"
+import tbfire from "../../public/images/HSR/tbfire.png"
+import tbphys from "../../public/images/HSR/tbphys.png"
+import welt from "../../public/images/HSR/welt.png"
+import xueyi from "../../public/images/HSR/xueyi.png"
+import yanqing from "../../public/images/HSR/yanqing.png"
+import yukong from "../../public/images/HSR/yukong.png"
+import na from "../../public/images/HSR/na.png"
+import TextInput from "./TextInput";
 
 const FullDropdown = (props) => {
   const characters = [
-  { text:"Acheron", star:5, type:"N", stag:13, mat:"NAC", boss:"B"},
-  { text:"Argenti", star:5, type:"E", stag:11, mat:"E", boss:"DS"},
-  { text:"Arlan", star:4, type:"D", stag:9, mat:"DSZ", boss:"D"},
-  { text:"Asta", star:4, type:"Ha", stag:5, mat:"HRS", boss:"D"},
-  { text:"Aventurine", star:5, type:"P", stag:15, mat:"", boss:"B"},
-  { text:"Bailu", star:5, type:"A", stag:9, mat:"A", boss:"EF"},
-  { text:"Black Swan", star:5, type:"N", stag:1, mat:"NAC", boss:"B"},
-  { text:"Blade", star:5, type:"D", stag:1, mat:"DSZ", boss:"DS"},
-  { text:"Boothill", star:5, type:"Hu", stag:0, mat:"", boss:""},
-  { text:"Bronya", star:5, type:"Ha", stag:14, mat:"HRS", boss:"EF"},
-  { text:"Clara", star:5, type:"D", stag:2, mat:"DSZ", boss:"EF"},
-  { text:"Dan Heng", star:4, type:"Hu", stag:14, mat:"H", boss:"D"},
-  { text:"Dan Heng: Imbibitor Lunae", star:5, type:"D", stag:15, mat:"DSZ", boss:"DS"},
-  { text:"Dr. Ratio", star:5, type:"Hu", stag:15, mat:"H", boss:"B"},
-  { text:"Fu Xuan", star:5, type:"P", stag:10, mat:"P", boss:"DS"},
-  { text:"Gallagher", star:4, type:"A", stag:0, mat:"A", boss:""},
-  { text:"Gepard", star:5, type:"P", stag:8, mat:"P", boss:"EF"},
-  { text:"Guinaifen", star:4, type:"N", stag:12, mat:"NGM", boss:"DS"},
-  { text:"Hanya", star:4, type:"Ha", stag:11, mat:"HRS", boss:"DS"},
-  { text:"Herta", star:4, type:"E", stag:8, mat:"E", boss:"D"},
-  { text:"Himeko", star:5, type:"E", stag:5, mat:"E", boss:"D"},
-  { text:"Hook", star:4, type:"D", stag:5, mat:"DSZ", boss:"EF"},
-  { text:"Huohuo", star:5, type:"A", stag:1, mat:"A", boss:"DS"},
-  { text:"Jing Yuan", star:5, type:"E", stag:13, mat:"E", boss:"D"},
-  { text:"Jingliu", star:5, type:"D", stag:6, mat:"DSZ", boss:"DS"},
-  { text:"Kafka", star:5, type:"N", stag:13, mat:"NGM", boss:"DS"},
-  { text:"Luka", star:4, type:"N", stag:2, mat:"NGM", boss:"DS"},
-  { text:"Luocha", star:5, type:"A", stag:7, mat:"A", boss:"EF"},
-  { text:"Lynx", star:4, type:"A", stag:10, mat:"A", boss:"DS"},
-  { text:"March 7th", star:4, type:"P", stag:8, mat:"P", boss:"D"},
-  { text:"Misha", star:4, type:"D", stag:4, mat:"DSW", boss:"B"},
-  { text:"Natasha", star:4, type:"A", stag:2, mat:"A", boss:"EF"},
-  { text:"Pela", star:4, type:"N", stag:8, mat:"NGM", boss:"EF"},
-  { text:"Qingque", star:4, type:"E", stag:16, mat:"E", boss:"EF"},
-  { text:"Robin", star:5, type:"H", stag:0, mat:"", boss:""},
-  { text:"Ruan Mei", star:5, type:"H", stag:6, mat:"HRS", boss:"B"},
-  { text:"Sampo", star:4, type:"N", stag:14, mat:"NGM", boss:"EF"},
-  { text:"Seele", star:5, type:"Hu", stag:16, mat:"H", boss:"EF"},
-  { text:"Serval", star:4, type:"E", stag:9, mat:"E", boss:"EF"},
-  { text:"Silver Wolf", star:5, type:"N", stag:16, mat:"NGM", boss:"D"},
-  { text:"Sparkle", star:5, type:"Ha", stag:3, mat:"HTR", boss:"B"},
-  { text:"Sushang", star:4, type:"Hu", stag:2, mat:"H", boss:"EF"},
-  { text:"Tingyun", star:4, type:"Ha", stag:9, mat:"HRS", boss:"D"},
-  { text:"Topaz", star:5, type:"Hu", stag:12, mat:"H", boss:"DS"},
-  { text:"Trailblazer: Preservation", star:5, type:"P", stag:0, mat:"", boss:"D"},
-  { text:"Trailblazer: Destruction", star:5, type:"D", stag:0, mat:"DSZ", boss:"D"},
-  { text:"Welt", star:5, type:"N", stag:7, mat:"NGM", boss:"D"},
-  { text:"Xueyi", star:4, type:"D", stag:10, mat:"DSZ", boss:"B"},
-  { text:"Yanqing", star:5, type:"Hu", stag:6, mat:"H", boss:"EF"},
-  { text:"Yukong", star:4, type:"Ha", stag:7, mat:"HRS", boss:"D"}
+  { text:"Acheron", star:5, type:"N", stag:13, mat:"NAC", boss:"B", image:na},
+  { text:"Argenti", star:5, type:"E", stag:11, mat:"E", boss:"DS", image:argenti},
+  { text:"Arlan", star:4, type:"D", stag:9, mat:"DSZ", boss:"D", image:arlan},
+  { text:"Asta", star:4, type:"Ha", stag:5, mat:"HRS", boss:"D", image:asta},
+  { text:"Aventurine", star:5, type:"P", stag:15, mat:"", boss:"B", image:na},
+  { text:"Bailu", star:5, type:"A", stag:9, mat:"A", boss:"EF", image:bailu},
+  { text:"Black Swan", star:5, type:"N", stag:1, mat:"NAC", boss:"B", image:blackswan},
+  { text:"Blade", star:5, type:"D", stag:1, mat:"DSZ", boss:"DS", image:blade},
+  { text:"Boothill", star:5, type:"Hu", stag:0, mat:"", boss:"", image:na},
+  { text:"Bronya", star:5, type:"Ha", stag:14, mat:"HRS", boss:"EF", image:bronya},
+  { text:"Clara", star:5, type:"D", stag:2, mat:"DSZ", boss:"EF", image:clara},
+  { text:"Dan Heng", star:4, type:"Hu", stag:14, mat:"H", boss:"D", image:danheng},
+  { text:"Dan Heng: Imbibitor Lunae", star:5, type:"D", stag:15, mat:"DSZ", boss:"DS", image:danhengil},
+  { text:"Dr. Ratio", star:5, type:"Hu", stag:15, mat:"H", boss:"B", image:drratio},
+  { text:"Fu Xuan", star:5, type:"P", stag:10, mat:"P", boss:"DS", image:fuxuan},
+  { text:"Gallagher", star:4, type:"A", stag:0, mat:"A", boss:"", image:na},
+  { text:"Gepard", star:5, type:"P", stag:8, mat:"P", boss:"EF", image:gepard},
+  { text:"Guinaifen", star:4, type:"N", stag:12, mat:"NGM", boss:"DS", image:guinaifen},
+  { text:"Hanya", star:4, type:"Ha", stag:11, mat:"HRS", boss:"DS", image:hanya},
+  { text:"Herta", star:4, type:"E", stag:8, mat:"E", boss:"D", image:herta},
+  { text:"Himeko", star:5, type:"E", stag:5, mat:"E", boss:"D", image:himeko},
+  { text:"Hook", star:4, type:"D", stag:5, mat:"DSZ", boss:"EF", image:hook},
+  { text:"Huohuo", star:5, type:"A", stag:1, mat:"A", boss:"DS", image:huohuo},
+  { text:"Jing Yuan", star:5, type:"E", stag:13, mat:"E", boss:"D", image:jingyuan},
+  { text:"Jingliu", star:5, type:"D", stag:6, mat:"DSZ", boss:"DS", image:jingliu},
+  { text:"Kafka", star:5, type:"N", stag:13, mat:"NGM", boss:"DS", image:kafka},
+  { text:"Luka", star:4, type:"N", stag:2, mat:"NGM", boss:"DS", image:luka},
+  { text:"Luocha", star:5, type:"A", stag:7, mat:"A", boss:"EF", image:luocha},
+  { text:"Lynx", star:4, type:"A", stag:10, mat:"A", boss:"DS", image:lynx},
+  { text:"March 7th", star:4, type:"P", stag:8, mat:"P", boss:"D", image:march},
+  { text:"Misha", star:4, type:"D", stag:4, mat:"DSW", boss:"B", image:misha},
+  { text:"Natasha", star:4, type:"A", stag:2, mat:"A", boss:"EF", image:natasha},
+  { text:"Pela", star:4, type:"N", stag:8, mat:"NGM", boss:"EF", image:pela},
+  { text:"Qingque", star:4, type:"E", stag:16, mat:"E", boss:"EF", image:qingque},
+  { text:"Robin", star:5, type:"H", stag:0, mat:"", boss:"", image:na},
+  { text:"Ruan Mei", star:5, type:"H", stag:6, mat:"HRS", boss:"B", image:ruanmei},
+  { text:"Sampo", star:4, type:"N", stag:14, mat:"NGM", boss:"EF", image:sampo},
+  { text:"Seele", star:5, type:"Hu", stag:16, mat:"H", boss:"EF", image:seele},
+  { text:"Serval", star:4, type:"E", stag:9, mat:"E", boss:"EF", image:serval},
+  { text:"Silver Wolf", star:5, type:"N", stag:16, mat:"NGM", boss:"D", image:silverwolf},
+  { text:"Sparkle", star:5, type:"Ha", stag:3, mat:"HTR", boss:"B", image:sparkle},
+  { text:"Sushang", star:4, type:"Hu", stag:2, mat:"H", boss:"EF", image:sushang},
+  { text:"Tingyun", star:4, type:"Ha", stag:9, mat:"HRS", boss:"D", image:tingyun},
+  { text:"Topaz", star:5, type:"Hu", stag:12, mat:"H", boss:"DS", image:topaz},
+  { text:"Trailblazer: Preservation", star:5, type:"P", stag:0, mat:"", boss:"D", image:tbfire},
+  { text:"Trailblazer: Destruction", star:5, type:"D", stag:0, mat:"DSZ", boss:"D", image:tbphys},
+  { text:"Welt", star:5, type:"N", stag:7, mat:"NGM", boss:"D", image:welt},
+  { text:"Xueyi", star:4, type:"D", stag:10, mat:"DSZ", boss:"B", image:xueyi},
+  { text:"Yanqing", star:5, type:"Hu", stag:6, mat:"H", boss:"EF", image:yanqing},
+  { text:"Yukong", star:4, type:"Ha", stag:7, mat:"HRS", boss:"D", image:yukong}
 ]
 
 const lightCones = [
@@ -166,8 +219,36 @@ const lightConesMapped = lightCones.map((lightCone, index) => (
   </DropdownItem>
 ));
 
-const [selectedCharacter, setSelectedCharacter] = useState(null);
+const [selectedCharacter, setSelectedCharacter] = useState(characters[2]);
 const [selectedLightCone, setSelectedLightCone] = useState(null);
+const [currentLevel, setCurrentLevel] = useState(1);
+const [desiredLevel, setDesiredLevel] = useState(1);
+const [currentLCLevel, setCurrentLCLevel] = useState(1);
+const [desiredLCLevel, setDesiredLCLevel] = useState(1);
+
+const handleCurrentLevelChange = (event) => {
+  setCurrentLevel(event.target.value);
+};
+
+const handleDesiredLevelChange = (event) => {
+  const newDesiredLevel = event.target.value;
+  if (newDesiredLevel < currentLevel) {
+    setCurrentLevel(newDesiredLevel);
+  }
+  setDesiredLevel(newDesiredLevel);
+}
+
+const handleCurrentLCLevelChange = (event) => {
+  setCurrentLCLevel(event.target.value);
+};
+
+const handleDesiredLCLevelChange = (event) => {
+  const newDesiredLCLevel = event.target.value;
+  if (newDesiredLCLevel < currentLCLevel) {
+    setCurrentLCLevel(newDesiredLCLevel);
+  }
+  setDesiredLCLevel(newDesiredLCLevel);
+}
 
 const filteredLightCones =
     selectedCharacter === null
@@ -184,9 +265,7 @@ const filteredLightCones =
   return (
     <>
       <div>
-        <p>Image: {selectedCharacter ? selectedCharacter.text : "None"}</p>
-        <p>Image: {selectedLightCone ? selectedLightCone.text : "None"}</p>
-
+        <p>{selectedCharacter ? <Image src={selectedCharacter.image} alt="pBook" className='border-2 border-solid object-center w-80 h-auto mx-12'/> : ""}</p>
         <Dropdown>
           <DropdownTrigger>
             <Button 
@@ -197,6 +276,10 @@ const filteredLightCones =
             </Button>
           </DropdownTrigger>
           <DropdownMenu 
+            style={{
+              maxHeight:'350px',
+              overflow:'scroll'
+            }}
             aria-label="Single selection example"
             variant="flat"
             disallowEmptySelection
@@ -223,6 +306,10 @@ const filteredLightCones =
             </Button>
           </DropdownTrigger>
           <DropdownMenu 
+            style={{
+              maxHeight:'200px',
+              overflow:'scroll'
+            }}
             aria-label="Single selection example"
             variant="flat"
             disallowEmptySelection
@@ -237,6 +324,42 @@ const filteredLightCones =
             {filteredLightCones}
           </DropdownMenu>
         </Dropdown>
+        <br/>
+        Current Char Level: <input
+          type="number"
+          min='1'
+          max='80'
+          value={currentLevel}
+          onChange={handleCurrentLevelChange}
+          className='w-12 justify-center text-center border-2'
+        />
+        <br/>
+        Desired Char Level: <input
+          type="number"
+          min='1'
+          max='80'
+          value={desiredLevel}
+          onChange={handleDesiredLevelChange}
+          className='w-12 justify-center text-center border-2'
+        />
+        <br/>
+        Current Light Cone Level: <input
+          type="number"
+          min='1'
+          max='80'
+          value={currentLCLevel}
+          onChange={handleCurrentLCLevelChange}
+          className='w-12 justify-center text-center border-2'
+        />
+        <br/>
+        Desired Light Cone Level: <input
+          type="number"
+          min='1'
+          max='80'
+          value={desiredLCLevel}
+          onChange={handleDesiredLCLevelChange}
+          className='w-12 justify-center text-center border-2'
+        />
       </div>
     </>
   );
--- src/pages/planner.js
+++ src/pages/planner.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import FullDropdown from '@/components/FullDropdown';
+import TextInput from '@/components/TextInput';
 
 const planner = () => {
     return (
         <>
-            <nav className='w-full px-32 flex justify-between'>
-                <FullDropdown/>
-                <FullDropdown/>
-                <FullDropdown/>
-                <FullDropdown/>
-            </nav>
+            {/* <nav className='w-full px-32 flex justify-between'></nav> */}
+            <table style={{width:'100%'}} className='w-full flex-row justify-between items-center object-center text-center text-lg text-bold border p-4 bg-light text-dark'>
+                <thead>
+                    <tr>
+                        <th><FullDropdown/></th>
+                        <th><FullDropdown/></th>
+                        <th><FullDropdown/></th>
+                        <th><FullDropdown/></th>
+                    </tr>
+                </thead>
+            </table>
         </>
     );
 };
